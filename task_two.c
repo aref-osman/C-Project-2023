9d03fd783a4f415b083a9cadf7c4e8dd2ab76094
GenuinelyAref <aref.osman18@gmail.com>
2023-10-18 11:46:03 +1300

    Task Two

--- task_two.c
+++ task_two.c
@@ -22,24 +22,19 @@ void PrintArray(int values[MAP_SIZE][MAP_SIZE]) {
 void InitialiseMap(int map[MAP_SIZE][MAP_SIZE]) {
 
     double x = 0;
-    int index = 0;
-    int ship_num = 0;
+    int ship_row = 0;
     int ship_lengths[4] = {2, 3, 4, 5};
 
-    for (int row = 0; row < MAP_SIZE; row++)
+    for (int ship_num = 0; ship_num < 4; ship_num++)
     {
-        x += (MAP_SIZE-1)/3;
-        index = (int)x;
-        printf("row: %d\nx: %f\nindex: %d\nship_num: %d\n\n", row, x, index, ship_num);
-        if (index == row)
-        {
-            for (int ship_block = 0; ship_block < ship_lengths[index]; ship_block++)
-            {
-                map[row][ship_block] = ship_lengths[ship_num];
-            }
-            ship_num += 1;
-        }        
+        
+        for (int ship_block = 0; ship_block < ship_lengths[ship_num]; ship_block++) {   
+            map[ship_row][ship_block] = ship_lengths[ship_num];
+        }
+
+        ship_row += (MAP_SIZE-1)/3;
     }
+    
     
 }
 
