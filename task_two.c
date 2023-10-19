21402558836f1fce1acbeb9f78dfdce8f84281a0
GenuinelyAref <aref.osman18@gmail.com>
2023-10-19 23:56:28 +1300

    minor fixes
    - initialise blank array
    - reposition 3-block ship to a valid position

--- task_two.c
+++ task_two.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-#define MAP_SIZE 15
+#define MAP_SIZE 6
 
 void PrintArray(int values[MAP_SIZE][MAP_SIZE]) {
     
@@ -21,22 +21,39 @@ void PrintArray(int values[MAP_SIZE][MAP_SIZE]) {
 
 void InitialiseMap(int map[MAP_SIZE][MAP_SIZE]) {
 
-    double x = 0;
     int ship_row = 0;
     int ship_lengths[4] = {2, 3, 4, 5};
 
-    for (int ship_num = 0; ship_num < 4; ship_num++)
+    // initialise blank array
+    for (int row = 0; row < MAP_SIZE; row++)
     {
-        
-        ship_row = ship_num*(MAP_SIZE-1)/3;
-
-        for (int ship_block = 0; ship_block < ship_lengths[ship_num]; ship_block++) {   
-            map[ship_row][ship_block] = ship_lengths[ship_num];
+        for (int col = 0; col < MAP_SIZE; col++)
+        {
+            map[row][col] = 0;
         }
-
+        
     }
     
-    
+
+    for (int ship_num = 0; ship_num < 4; ship_num++)
+    {
+        // place the 3 block ship vertically next to the 2 block ship, 3 blocks away
+        if (ship_num == 1)
+        {
+            ship_row = (ship_num-1)*(MAP_SIZE-1)/3;
+            map[ship_row][5] = 3;
+            map[ship_row + 1][5] = 3;
+            map[ship_row + 2][5] = 3;
+        }
+        // linearly space all the other ships
+        else
+        {
+            ship_row = ship_num*(MAP_SIZE-1)/3;
+            for (int ship_block = 0; ship_block < ship_lengths[ship_num]; ship_block++) {   
+                map[ship_row][ship_block] = ship_lengths[ship_num];
+            }
+        }
+    }
 }
 
 int main(void){
